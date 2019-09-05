220d8cee27945140a83c09c9022a14add1253f41
johnsoncheg <johnsoncheg@163.com>
2019-09-05 10:56:56 +0800

    fix: fix spell bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+
 import Apng from './apng.vue'
 
 function install (vue) {
   if (install.installed) return
   install.installed = true
-  Vue.component('v-apng', Apng)
+  vue.component('v-apng', Apng)
 }
 
 Apng.install = install
